2dcbea77fe72e3218a20f356cbdeb3222912b9b3
Euge <meuparodi@gmail.com>
2023-02-09 14:52:26 -0300

    paginas base del sitio

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+
+"use strict";
+
+document.querySelector(".iconoMenu").addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    document.querySelector("nav").classList.toggle("show");
+}
+
