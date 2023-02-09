71c20bbac1a0ae67c095e4a28cf738c9a8a8879a
Euge <meuparodi@gmail.com>
2023-02-09 16:16:20 -0300

    agrgue link reservas

--- js/menu.js
+++
@@ -1,9 +0,0 @@
-
-"use strict";
-
-document.querySelector(".iconoMenu").addEventListener("click", toggleMenu);
-
-function toggleMenu() {
-    document.querySelector("nav").classList.toggle("show");
-}
-
