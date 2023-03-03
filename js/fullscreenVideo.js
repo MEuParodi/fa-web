2e17eb83ab6cd28cd1fc680e72ab2403774a1225
saguero <sergiogaguero@gmail.com>
2023-03-03 18:18:00 -0300

    agregamos un monton de cosas

---
+++ js/fullscreenVideo.js
@@ -0,0 +1,45 @@
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    videoId: 'H1gn4K4qfBU',
+    playerVars: {
+        'autoplay':1,
+        'controls':0,
+        'rel':0,
+        'showinfo':0,
+        'autohide':1,
+        'modestbranding':1,
+        'mute':1,
+        'loop':1,
+        'disablekb':1,
+        'playsinline':1,
+    }
+
+
+  });
+}
+
+const closeButton  = document.getElementById('buttonClose');
+const scrollContainer1  = document.getElementById('scrollContainer');
+const fullscreen = document.getElementById('fullscreen');
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+        closeVideo()
+    }
+});
+
+closeButton.addEventListener('click', function(event) { 
+    closeVideo()
+});
+
+scrollContainer1.addEventListener('click', function(event) { 
+    player.seekTo(0);
+    player.playVideo();  
+    fullscreen.classList.toggle('show');
+});
+
+function closeVideo() {
+    player.stopVideo();
+    fullscreen.classList.remove('show');
+}
